79e31b467fca18bfdc9124632c2e55a317655ea4
Elena176 <lena.tdn176@gmail.com>
2021-09-20 12:32:19 +0300

    added instance

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,34 +1,26 @@
 import axios from 'axios';
 import {DataUsersPropsType, FollowUserPropsType, ProfileUserType} from '../redux/store';
 
-const baseUrl = `https://social-network.samuraijs.com/api/1.0/`;
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: `https://social-network.samuraijs.com/api/1.0/`,
+    headers: {
+    'API-KEY':'5d9e2ad6-9759-43e9-a5e1-226b020868e0'
+},
+});
+
 export const getUsers = (currentPage: number, pageSize: number) => {
-   return axios.get<DataUsersPropsType>(baseUrl + `users?page=${currentPage}&count=${pageSize}`, {
-        withCredentials: true
-    }).then(response => response.data)
+   return instance.get<DataUsersPropsType>(`users?page=${currentPage}&count=${pageSize}`).then(response => response.data)
 };
 
 export const deleteUser = (id: number) => {
-    return  axios.delete<FollowUserPropsType> (
-        baseUrl + `follow/${id}`,{
-            withCredentials: true,
-            headers: {
-                'API-KEY':'5d9e2ad6-9759-43e9-a5e1-226b020868e0'
-            }
-        }).then(response => response.data)
+    return  instance.delete<FollowUserPropsType> (`follow/${id}`).then(response => response.data)
 };
 
 export const followUser = (id: number) => {
-   return axios.post<FollowUserPropsType>(
-        baseUrl + `follow/${id}`, {}, {
-            withCredentials: true,
-            headers: {
-                'API-KEY':'5d9e2ad6-9759-43e9-a5e1-226b020868e0'
-            }
-        }).then(response => response.data)
+   return instance.post<FollowUserPropsType>(`follow/${id}`).then(response => response.data)
 };
 
 export const getProfile = (userId: string) => {
-    return  axios.get<ProfileUserType>(baseUrl + `profile/` + userId)
-        .then(response => response.data)
+    return  instance.get<ProfileUserType>( `profile/` + userId).then(response => response.data)
 };
