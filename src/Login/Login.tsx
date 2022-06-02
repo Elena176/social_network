7195a69c5bd07a7c3e59ac6e6b4181d3c06a8bef
Elena176 <lena.tdn176@gmail.com>
2022-06-02 12:00:55 +0300

    refactored LoginForm

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Field, Form, Formik} from 'formik';
-import {Input} from '../components/common/FormsControl/FormsControl';
+import {createField, Input} from '../components/common/FormsControl/FormsControl';
 import {validateItem} from '../utils/validators/validators';
 import {connect} from 'react-redux';
 import {logIn} from '../redux/auth-reducer';
@@ -10,83 +10,77 @@ import s from './../components/common/FormsControl/FormsControls.module.css';
 
 const validateLoginForm = validateItem(30)
 
-export const LoginFormFormik = (props: LoginFormPropsType) => {
-    const submit = (values: FormDataType, {setSubmitting}: { setSubmitting: (isSubmitting: boolean) => void }) => {
-        props.onSubmit(values)
-        setSubmitting(false)
-    }
-    return <div>
-        <Formik
-            initialValues={{email: '', password: '', rememberMe: false}}
-            onSubmit={submit}
-        >
-            {({isSubmitting}) => (
-                <Form>
-                    <div>
-                        <Field component={Input} type={'text'} validate={validateLoginForm} name={'email'}
-                               placeholder={'email'}/>
-                    </div>
-                    <div>
-                        <Field component={Input} type={'password'} validate={validateLoginForm} name={'password'}
-                               placeholder={'password'}/>
-                    </div>
-                    <label className={''}>
-                        <Field component={Input} type={'checkbox'} name={'rememberMe'}/>
-                        remember me
-                    </label>
-                    <div>
-                        {props.error && <div className={s.errorMessage}> {props.error} </div>}
-                    </div>
-                    <div>
-                        <button type="submit" disabled={isSubmitting}>Login</button>
-                    </div>
-                </Form>
-            )}
-        </Formik>
-    </div>
+export const LoginFormFormik: React.FC<LoginFormPropsType> = ({onSubmit, error}) => {
+  const submit = (values: FormDataType, {setSubmitting}: { setSubmitting: (isSubmitting: boolean) => void }) => {
+    onSubmit(values)
+    setSubmitting(false)
+  }
+  return <div>
+    <Formik
+      initialValues={{email: '', password: '', rememberMe: false}}
+      onSubmit={submit}
+    >
+      {({isSubmitting}) => (
+        <Form>
+          {createField(Input, 'text', validateLoginForm, 'email', 'email')}
+          {createField(Input, 'password', validateLoginForm, 'password', 'password')}
+          <label className={''}>
+            <Field component={Input} type={'checkbox'} name={'rememberMe'}/>
+            remember me
+          </label>
+          <div>
+            {error && <div className={s.errorMessage}> {error} </div>}
+          </div>
+          <div>
+            <button type="submit" disabled={isSubmitting}>Login</button>
+          </div>
+        </Form>
+      )}
+    </Formik>
+  </div>
 }
 
-const Login = (props: LoginPropsType) => {
-    const onSubmit = (formData: FormDataType) => {
-        props.logIn(formData.email, formData.password, formData.rememberMe)
-    }
-    if (props.isAuth) {
-        return <Redirect to={'/profile'}/>
-    }
-    return <div>
-        <h1>LOGIN</h1>
-        <LoginFormFormik onSubmit={onSubmit} error={props.error}/>
-    </div>
+const Login: React.FC<LoginPropsType> = ({isAuth, error, logIn}) => {
+  const onSubmit = (formData: FormDataType) => {
+    logIn(formData.email, formData.password, formData.rememberMe)
+  }
+  if (isAuth) {
+    return <Redirect to={'/profile'}/>
+  }
+  return <div>
+    <h1>LOGIN</h1>
+    <LoginFormFormik onSubmit={onSubmit} error={error}/>
+  </div>
 }
 
 const mapStateToProps = (state: AppStateType) => {
-    return {
-        isAuth: state.auth.isAuth,
-        error: state.auth.error,
-    }
+  return {
+    isAuth: state.auth.isAuth,
+    error: state.auth.error,
+  }
 }
 export default connect<MapStatePropsType, MapDispatchPropsType, {}, AppStateType>(mapStateToProps, {logIn})(Login);
 
 
 //types
 type FormDataType = {
-    email: string
-    password: string
-    rememberMe: boolean
+  email: string
+  password: string
+  rememberMe: boolean
 }
 
 type LoginFormPropsType = {
-    onSubmit: (formData: FormDataType) => void
-    error: string | null
+  onSubmit: (formData: FormDataType) => void
+  error: string | null
 }
 
 type MapStatePropsType = {
-    isAuth: boolean
-    error: string | null
+  isAuth: boolean
+  error: string | null
 }
 
 type MapDispatchPropsType = {
-    logIn: (email: string, password: string, rememberMe: boolean) => void
+  logIn: (email: string, password: string, rememberMe: boolean) => void
 }
 
 type LoginPropsType = MapStatePropsType & MapDispatchPropsType;
--- src/components/common/FormsControl/FormsControl.tsx
+++ src/components/common/FormsControl/FormsControl.tsx
@@ -1,34 +1,43 @@
 import s from './FormsControls.module.css'
+import {Field} from 'formik';
+import React from 'react';
 
-export const Textarea = ({field, form: { touched, errors, isValid}, ...props}: any) => {
-    return (
-        <div>
+export const Textarea = ({field, form: {touched, errors, isValid}, ...props}: any) => {
+  return (
+    <div>
             <textarea
-                {...field} {...props}
-                className={
-                    !isValid && touched[field.name] && errors[field.name] ? s.error : ""
-                }
+              {...field} {...props}
+              className={
+                !isValid && touched[field.name] && errors[field.name] ? s.error : ''
+              }
             />
-            {touched[field.name] && errors[field.name] && (
-                <div className={s.errorMessage}>{errors[field.name]}</div>
-            )}
-        </div>
-    )
+      {touched[field.name] && errors[field.name] && (
+        <div className={s.errorMessage}>{errors[field.name]}</div>
+      )}
+    </div>
+  )
 }
 
-export const Input = ({field, form: { touched, errors, isValid}, ...props}: any) => {
+export const Input = ({field, form: {touched, errors, isValid}, ...props}: any) => {
 
-    return (
-        <div>
-            <input
-                {...field} {...props}
-                className={
-                    !isValid && touched[field.name] && errors[field.name] ? s.error : ""
-                }
-            />
-            { !isValid && touched[field.name] && errors[field.name] && (
-                <div className={s.errorMessage}>{errors[field.name]}</div>
-            )}
-        </div>
-    )
+  return (
+    <div>
+      <input
+        {...field} {...props}
+        className={
+          !isValid && touched[field.name] && errors[field.name] ? s.error : ''
+        }
+      />
+      {!isValid && touched[field.name] && errors[field.name] && (
+        <div className={s.errorMessage}>{errors[field.name]}</div>
+      )}
+    </div>
+  )
 }
+
+export const createField = (component: any, type: string, validate: (values: string) => void, name: string,  placeholder: string) => (
+  <div>
+  <Field component={component} type={type} validate={validate} name={name}
+         placeholder={placeholder}/>
+  </div>
+)
